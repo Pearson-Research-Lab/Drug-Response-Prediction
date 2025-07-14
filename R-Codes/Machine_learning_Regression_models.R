72c1bf44e917f1c4658040115c8302105dbea475
ShilpaGarg93 <80582989+ShilpaGarg93@users.noreply.github.com>
2025-07-14 11:38:09 +0100

    Update Machine_learning_Regression_models.R

--- R-Codes/Machine_learning_Regression_models.R
+++ R-Codes/Machine_learning_Regression_models.R
@@ -21,7 +21,8 @@ var_log <- c("BMI", "Total_Chol", ...)
 df_log[var_log] <- lapply(..., log)
 
 # Initial Full Linear Model
-lm(Diff_HbA1c ~ ., data = df_log)
+full_model <- lm(Diff_HbA1c ~ ., data = df_log)
+summary(full_model)
 
 # Min-Max Scaling (for ML models)
 min_max_scaling <- function(x) ...
@@ -31,3 +32,41 @@ df_scaled[continuous_vars] <- lapply(..., min_max_scaling)
 train_index <- createDataPartition(...)
 train_data_lm <- selected_data[train_index, ]
 test_data_lm <- selected_data[-train_index, ]
+
+# Model Training and Evaluation
+# Linear Regression (CV = 10)
+lm_model <- train(..., method = "lm", trControl = trControl_lm)
+
+# Random Forest
+rf_model <- train(..., method = "rf", ...)
+
+# Support Vector Machine (SVM)
+svm_model <- train(..., method = "svmRadial", ...)
+
+# XGBoost
+dmy <- dummyVars(" ~ .", data = selected_data)
+trsf <- data.frame(predict(...))
+
+rwCV <- xgb.cv(...)
+xgb_model <- xgboost(..., nrounds = 200)
+
+# Artificial Neural Network (ANN)
+model <- neuralnet(Diff_HbA1c ~ ., ...)
+
+# Bayesian Additive Regression Trees (BART)
+bart_model <- wbart(train_x, train_y, test_x)
+
+# Results Aggregation
+results_data <- data.frame("Models" = c(...), "RMSE", "MAE", "R_Squared")
+
+
+
+
+
+
+
+
+
+
+
+
